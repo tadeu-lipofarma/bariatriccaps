0fb89625da3c22e937b0c47e99a4c297bce48524
Gustavo Bragaia <gustavobragaia12@gmail.com>
2021-01-29 18:30:40 -0300

    integrações

---
+++ assets/popup/lobi-content.js
@@ -0,0 +1,151 @@
+var IMG_PREFIX = 'https://i.ibb.co/fQfpypM/';
+//LobiBox default options
+Lobibox.notify.DEFAULTS = $.extend({}, Lobibox.notify.DEFAULTS, {
+    sound: false,
+    position: 'bottom left',
+    title: false,
+    delay: 5000,
+    messageHeight: 90,
+});
+(function() {
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gabriel Lima</b> acabou de comprar 3 potes. Restam <b>apenas 17 unidades</b> no estoque!'
+        });
+    }, 7000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 6 pessoas compraram nos últimos 15 minutos.</b>  <br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 15000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Luan Souza</b> acabou de comprar 5 potes.</br> Restam <b>apenas 11 unidades</b> no estoque!'
+        });
+    }, 23000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram comprar o Bariatric </b>.<br><br><b>Clique</b> e garanta o também o seu...'
+        });
+    }, 31000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Julio Souza</b> acabou de comprar 3 potes.  Restam <b>apenas 8 unidades</b> no estoque!'
+        });
+    }, 39000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Sergio Abrãao</b> acabou de comprar 5 potes.  Restam <b>apenas 5 unidades</b> no estoque!'
+        });
+    }, 47000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gabriel Monteiro</b> acabou de comprar 1 pote.  Restam <b>apenas 4 unidades</b> no estoque!'
+        });
+    }, 55000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 pessoas acabaram de comprar o Bariatric </b>.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 62000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Camilo Santos</b> acabou de comprar 1 Pote.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 70000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Paulo Souza</b> acabou de comprar 1 Pote.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 78000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram o Bariatric  nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 86000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gerson Silva</b> acabou de comprar 3 potes. Restam <b>apenas 17 unidades</b> no estoque!'
+        });
+    }, 94000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 6 pessoas compraram nos últimos 15 minutos.</b>  <br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 102000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Breno Duarte</b> acabou de comprar 5 potes.</br> Restam <b>apenas 11 unidades</b> no estoque!'
+        });
+    }, 110000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram comprar o Bariatric </b>.<br><br><b>Clique</b> e garanta o também o seu...'
+        });
+    }, 118000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Rafael Souza</b> acabou de comprar 3 potes.  Restam <b>apenas 8 unidades</b> no estoque!'
+        });
+    }, 126000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Liomar Abrãao</b> acabou de comprar 5 potes.  Restam <b>apenas 5 unidades</b> no estoque!'
+        });
+    }, 134000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Marcelo Victor</b> acabou de comprar 1 pote.  Restam <b>apenas 4 unidades</b> no estoque!'
+        });
+    }, 142000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 pessoas acabaram de comprar o Bariatric </b>.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 150000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>PaulaoRoberto</b> acabou de comprar 1 Pote.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 158000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Geovan Nunes</b> acabou de comprar 1 Pote.<br><br><b>Clique</b> e garanta o seu...'
+        });
+    }, 166000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram o Bariatric  nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta também o seu...'
+        });
+    }, 174000);
+})();
